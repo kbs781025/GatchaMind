4926e2a64047c68b0caab4315658e406de0ccdd7
kbs781025 <kbs781025@naver.com>
2020-03-25 21:30:13 +0900

    server setting done to servicing static files

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
 import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 4000;
+
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
