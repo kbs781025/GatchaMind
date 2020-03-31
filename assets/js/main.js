913aee80f2d01239ba7fdebaf61335e66eb6a8af
kbs781025 <kbs781025@naver.com>
2020-03-31 20:41:00 +0900

    simple login form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
-import chat from "./chat";
+const loginForm = document.getElementById("jsLoginForm");
+const body = document.body;
+const USER_NICKNAME = "nickname";
+const LOGGED_IN = "logged_in";
+const LOGGED_OUT = "logged_out";
 
-console.log("Hi");
+const nickName = localStorage.getItem(USER_NICKNAME);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const input = event.target.querySelector("input");
+  const inputValue = input.value;
+  if (inputValue) {
+    localStorage.setItem(USER_NICKNAME, inputValue);
+    input.value = "";
+  }
+}
+
+if (nickName) {
+  body.className = LOGGED_IN;
+} else {
+  body.className = LOGGED_OUT;
+}
+
+if (loginForm) {
+  loginForm.addEventListener("submit", handleFormSubmit);
+}
